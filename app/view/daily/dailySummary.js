ab36596172f6ec4cd108a4cbd3afe1c7f6077245
Aditia Nugraha <AtsukoAditia@gmail.com>
2014-10-29 16:43:47 +0700

    Ngerapihin Table menggunakan Bold karena memakai FontSize tidak bisa

--- app/view/daily/dailySummary.js
+++ app/view/daily/dailySummary.js
@@ -15,55 +15,65 @@ Ext.define('vts.view.daily.dailySummary', {
             },
 
         defaults: {
-            bodyStyle: 'padding:20px',
-            align : 'center'
+            bodyStyle: 'padding:10px',
             },
 
     items: [
                 {
+                    id          : 'tbl-all',
                     html        : 'Total Daily Fuel',
                     colspan     : 2,
                     dataIndex   : ''
                 },
                 {
+                    id      : 'tbl-all1',
                     html: 'NaN Liters',
                     colspan : 2,
                     dataIndex   : ''
                 },
                 {
+                    id      : 'tbl-all2',
                     html: 'Engine#1',
                     dataIndex   : ''
                 },
                 {
+                    id      : 'tbl-all3',
                     html: 'Engine#2',
                     dataIndex   : ''
                 },
                 {
+                    id      : 'tbl-all4',
                     html: 'NaN Liters',
                     dataIndex   : ''
                 },
                 {
+                    id      : 'tbl-all5',
                     html: 'NaN Liters',
                     dataIndex   : ''
                 },
                 {
+                    id      : 'tbl-all6',
                     html: 'Genset Daily Running Hours',
                     colspan : 2,
                     dataIndex   : ''
                 },
                 {
+                    id      : 'tbl-all7',
                     html: 'Genset#1',
                     dataIndex   : ''
                 },
                 {
+                    id      : 'tbl-all8',
                     html: 'Genset#2',
                     dataIndex   : ''
                 },
                 {
+                    id      : 'tbl-all9',
                     html: '0 Hours',
                     dataIndex   : ''
                 },
                 {
+                    id      : 'tbl-all10',
                     html: '0 Hours',
                     dataIndex   : ''
                 }

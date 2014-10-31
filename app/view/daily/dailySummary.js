1a629810a788d78fd774866c6bb8924b8ee7ff9f
Aditia Nugraha <AtsukoAditia@gmail.com>
2014-10-31 14:06:02 +0700

    Table sudah berdapingan dengan panel akumulasi menggunakan flex dan tinggal memasukan data

--- app/view/daily/dailySummary.js
+++ app/view/daily/dailySummary.js
@@ -3,7 +3,7 @@ Ext.define('vts.view.daily.dailySummary', {
     xtype       : 'xdaily',
 
     title   : 'Daily Summary',
-    split : true,
+    // split : true,
 
         layout: {
             type: 'table',

72306a6154979a82f4ba5f46ad83010885f90f75
Aditia Nugraha <AtsukoAditia@gmail.com>
2014-10-31 14:04:40 +0700

    sudah bisa menggunakan column di dalam column jadi hanya tinggal memasukan data

--- app/view/daily/Akumulasi.js
+++ app/view/daily/Akumulasi.js
@@ -1,75 +1,67 @@
 Ext.define('vts.view.daily.Akumulasi', {
-    extend      : 'Ext.panel.Panel',
+    extend      : 'Ext.grid.Panel',
     xtype       : 'xdaily2',
+    requires    : [],
 
-    title	: 'Akumulasi Data Flowmeter',
-	
-    	items : [{
-    		xtype	: 'gridpanel',
-    			columns : [{
-    				text : 'Date',
-    				dataIndex : ''
-    			},{
-    				text : 'Hour',
-    				dataIndex : ''
-    			},{
-    				text : 'Engine#1',
-    				align : 'center',
-    				width : 'auto',
-    					items : [{
-    						xtype : 'gridcolumn',
-    						text : 'Engine#1 (avg)',
-    						dataIndex : ''
-    					},{
-    						xtype : 'gridcolumn',
-    						text : 'Propeler#1 (avg)',
-    						dataIndex : ''
-    					},{
-    						xtype : 'gridcolumn',
-    						text : 'Flowmeter (avg)',
-    						dataIndex : ''
-    					},{
-    						xtype : 'gridcolumn',
-    						text : 'Overflow#1 (avg)',
-    						dataIndex : ''
-    					},{
-    						xtype : 'gridcolumn',
-    						text : 'Temperature#1 (avg)',
-    						dataIndex : ''
-    					},{
-    						xtype : 'gridcolumn',
-    						text : 'Pressure#1 (avg)',
-    						dataIndex : ''
-    					}],
-    			},{
-    				text : 'Engine#2',
-    				align : 'center',
-    				width : 'auto',
-    					items : [{
-    						xtype : 'gridcolumn',
-    						text : 'Engine#2 (avg)',
-    						dataIndex : ''
-    					},{
-    						xtype : 'gridcolumn',
-    						text : 'Propeler#2 (avg)',
-    						dataIndex : ''
-    					},{
-    						xtype : 'gridcolumn',
-    						text : 'Flowmeter (avg)',
-    						dataIndex : ''
-    					},{
-    						xtype : 'gridcolumn',
-    						text : 'Overflow#2 (avg)',
-    						dataIndex : ''
-    					},{
-    						xtype : 'gridcolumn',
-    						text : 'Temperature#2 (avg)',
-    						dataIndex : ''
-    					},{
-    						xtype : 'gridcolumn',
-    						text : 'Pressure#2 (avg)',
-    						dataIndex : ''
-    					}],
-    			}],
-    	}]
+    title :'Akumulasi Data Flowmeter',
+	// store : '',
+    columnLines : true,
+    columns : [{
+            text : 'Date',
+            dataIndex : ''
+        },{
+            text : 'Hour',
+            dataIndex : ''
+        },{
+            text : 'Engine#1',
+            columns : [{
+                text : 'Engine#1 (avg)',
+                dataIndex : ''
+            },{
+                text : 'Propeler#1 (avg)',
+                dataIndex : ''
+            },{
+                text : 'Flowmeter (avg)',
+                dataIndex : ''
+            },{
+                text : 'Overflow#1 (avg)',
+                dataIndex : ''
+            },{
+                text : 'Temperature#1 (avg)',
+                dataIndex : ''
+            },{
+                text : 'Pressure#1 (avg)',
+                dataIndex : ''
+            }],
+        },{
+            text : 'Engine#2',
+            columns : [{
+                text : 'Engine#2 (avg)',
+                dataIndex : ''
+            },{
+                text : 'Propeler#2 (avg)',
+                dataIndex : ''
+            },{
+                text : 'Flowmeter (avg)',
+                dataIndex : ''
+            },{
+                text : 'Overflow#2 (avg)',
+                dataIndex : ''
+            },{
+                text : 'Temperature#2 (avg)',
+                dataIndex : ''
+            },{
+                text : 'Pressure#2 (avg)',
+                dataIndex : ''
+            }],
+        },{
+            text : 'Genset Run-Hour',
+            columns : [{
+                text : 'Genset#1',
+                dataIndex : ''
+            },{
+                text : 'Genset#2',
+                dataIndex : ''
+            }]
+        }]
 })

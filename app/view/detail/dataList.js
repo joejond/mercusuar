91ba49bb164cb4c97ca1023b9a204c8d6c53580b
Aditia Nugraha <AtsukoAditia@gmail.com>
2014-10-31 14:07:32 +0700

    panel datalist sudah berdampingan dengan panel visualdata dan hanya tinggal memasukan data

--- app/view/detail/dataList.js
+++ app/view/detail/dataList.js
@@ -1,142 +1,98 @@
 Ext.define('vts.view.detail.dataList', {
-	extend		: 'Ext.panel.Panel',
+	extend		: 'Ext.grid.Panel',
 	xtype   	: 'xdetail',
-	requires	: ['vts.view.detail.visualData','vts.store.WsData'],
-    
-    layout: {
-        type	: 'hbox',
-        align	: 'stretch'
-    	},
-
-    items: [{               
-		store	: 'WsData',
-    xtype : 'grid',
-    title   : 'Data List',
-			columns: [{
-                    text : 'Time',
-                    dataIndex :'data_time'
-                    },{
-                        text    : 'Data Satelit',
-                        align   : 'center',
-                        width   : 'auto',
-                            items : [{
-                                text : 'Latitude',
-                                width : 100,
-                                dataIndex : 'lat'
-                            },{
-                                text : "longitude",
-                                dataIndex : 'lon'
-                            },{
-                                text : "speed",
-                                dataIndex : 'speed'
-                            },{
-              
-                                text : "heading",
-                                dataIndex : 'heading'
-                            	}],
-                         	},{
-                        text	: 'Data Engine#1',
-                        align	: 'center',
-                        width	: 'auto',
-                          	items : [{
-            
-                          		text : 'rpm#1',
-                          		dataIndex : 'rpm1'
-                          	},{
-            
-                          		text : 'prop#1',
-                          		dataIndex : 'prop1'
-                          	},{
-            
-                          		text : 'flowmeter#1',
-                          		dataIndex : 'flow1_olah'
-                          	},{
-            
-                          		text : 'overflow#1',
-                          		dataIndex : 'overflow1_olah'
-                          	},{
-            
-                          		text : 'temp#1',
-                          		dataIndex : 'temp1'
-                          	},{
-            
-                          		text : 'press#1',
-                          		dataIndex : 'press1'
-                          		}],
-                          	},{
-                        text	: 'Data Engine#2',
-                       	align	: 'center',
-                       	width	: 'auto',
-                       		items : [{
-          
-                        		text : 'rpm#2',
-                        		dataIndex : 'rpm2'
-                        	},{
-          
-                        		text : 'prop#2',
-                        		dataIndex : 'prop2'
-                        	},{
-          
-                        		text : 'flowmeter#2',
-                        		dataIndex : 'flow2_olah'
-                        	},{
-          
-                        		text : 'overflow#2',
-                        		dataIndex : 'overflow2_olah'
-                        	},{
-          
-                        		text : 'temp#2',
-                        		dataIndex : 'temp2'
-                        	},{
-          
-                        		text : 'press#2',
-                        		dataIndex : 'press2'
-                        		}],
-                          	},{
-                          	text	: 'Data Genset',
-                          	align	: 'center',
-                          	width	: 'auto',
-                          		items : [{
-              
-                          			text : 'Runhour#1',
-                          			dataIndex : 'runhour1_olah'
-                          		},{
-              
-                          			text : 'Runhour#2',
-                          			dataIndex : 'runhour2_olah'
-                          			}],
-                          		},{
-                          	text	: 'Data Panel',
-                          	align	: 'center',
-                          	width	: 'auto',
-                          		items : [{
-              
-                          			text : 'Battery',
-                          			dataIndex : 'battery'
-                          		},{
-              
-                          			text : 'Charger',
-                          			dataIndex : 'charger'
-                          			}],
-                          		},{
-                          	text	: 'Data Source',
-                          	align	: 'center',
-                          	width	: 'auto',
-                          		items : [{
-              
-                          			text : 'ID Kapal',
-                          			dataIndex : ''
-                          		},{
-              
-                          			text : 'ID Modem',
-                          			dataIndex : ''
-                          			}]
-                          	}],
-    flex : 1
-				},{
-					xtype	: 'splitter'   		
-				},{                    
-					xtype	: 'xdetail2',
-					flex: 2            
-					}]
+	requires	: ['vts.view.detail.visualData'],
+   
+  title: 'Data List',
+  store: 'WsData',
+  columnLines: true,
+   columns: [
+          {
+            text : 'Time',
+            dataIndex: 'data_time'
+        },{
+            text : 'Data Satelit',
+            columns: [{
+              text : 'Latitude',
+              dataIndex : 'lat'
+            },{
+              text : 'Longitude',
+              dataIndex : 'lon'
+            },{
+              text : 'Speed',
+              dataIndex : 'speed'
+            },{
+              text : 'Heading',
+              dataIndex : 'heading'
+            }]
+        },{
+          text : 'Engine#1',
+          columns : [{
+            text : 'rpm#1',
+            dataIndex : 'rpm1'
+          },{
+            text : 'prop#1',
+            dataIndex : 'prop1'
+          },{
+            text : 'flowmeter#1',
+            dataIndex : 'flow1_olah'
+          },{
+            text : 'overflow#1',
+            dataIndex : 'overflow1_olah'
+          },{
+            text : 'temp#1',
+            dataIndex : 'temp1'
+          },{
+            text : 'press#1',
+            dataIndex : 'press1'
+          }]
+        },{
+          text : 'Engine#2',
+          columns : [{
+            text : 'rpm#2',
+            dataIndex : 'rpm2'
+          },{
+            text : 'prop#2',
+            dataIndex : 'prop2'
+          },{
+            text : 'flowmeter#2',
+            dataIndex : 'flow2_olah'
+          },{
+            text : 'overflow#2',
+            dataIndex : 'overflow2_tot'
+          },{
+            text : 'temp#2',
+            dataIndex : 'temp2'
+          },{
+            text : 'press#2',
+            dataIndex : 'press2'
+          }]
+        },{
+          text : 'Data Genset',
+          columns : [{
+            text : 'runhour#1',
+            dataIndex : 'runhour1_olah'
+          },{
+            text : 'runhour#2',
+            dataIndex : 'runhour2_olah'
+          }]
+        },{
+          text : 'Data Panel',
+          columns : [{
+            text : 'Battery',
+            dataIndex : 'battery'
+          },{
+            text : 'Charger',
+            dataIndex : 'charger'
+          }]
+        },{
+          text : 'Data Source',
+          columns : [{
+            text : 'ID Kapal',
+            dataIndex : 'id_ship'
+          },{
+            text : 'ID Modem',
+            dataIndex : 'modem_id'
+          }]
+        }]
 })
